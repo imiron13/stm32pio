7cd0ef9a97244d4f972a5fc2f2abc8fd96b16d26
Igor <imironkbr@gmail.com>
2024-10-22 00:44:24 +0200

    Workaround for USB reset on STM32F103ZE

--- src/platform/cube_mx_stm32f103ze/gpio.c
+++ src/platform/cube_mx_stm32f103ze/gpio.c
@@ -65,6 +65,17 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOG, GPIO_PIN_11, GPIO_PIN_SET);
 
+  /* Configure PA12 (USB DP as GPIO output low)
+     This is a workaround to notify host about
+     reseting the USB port
+  */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
+  GPIO_InitStruct.Pin = GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /*Configure GPIO pin : PE4 */
   GPIO_InitStruct.Pin = GPIO_PIN_4;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
